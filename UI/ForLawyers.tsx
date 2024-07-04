86e5a4d084f369d24f931268d9abb5c0b850df0e
kmanish1 <kmanishreddy11@gmail.com>
2024-07-04 15:04:23 +0530

    for lawyers page

---
+++ UI/ForLawyers.tsx
@@ -0,0 +1,26 @@
+import ForlawCards from "@/components/ForlawCards"
+
+export default function ForLawyers(){
+    return(
+        <div className="bg-white py-8">
+            <div className="mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl">
+                <div className="text-center mx-auto space-y-6">
+                    <h3 className="text-2xl md:text-3xl font-bold">For Lawyers</h3>
+                    <p className="text-xl md:text-2xl">Case Management System built for India</p>
+                    <p className="text-[#1e3a8a] font-bold text-lg">Add-Assign-Track-Record</p>
+                </div>
+                <div className="flex justify-evenly items-center flex-wrap my-6">
+                    <div className="grid md:grid-cols-1 grid-cols-2 gap-6">
+                        <ForlawCards name={"DOCS"} img={"/forlawyers/ic1Doc.png"}/>
+                        <ForlawCards name={"TimeSheets"} img={"/forlawyers/ic2Doc.png"}/>
+                        <ForlawCards name={"Dashboard"} img={"/forlawyers/ic3Doc.png"}/>
+                        <ForlawCards name={"Lists"} img={"/forlawyers/ic4Doc.png"}/>
+                    </div>
+                    <div className="md:w-1/2 mt-6 md:mt-0">
+                        <img src="/forlawyers dashboard.png" className="w-full h-auto rounded-lg shadow-lg"></img>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ components/ForlawCards.tsx
@@ -0,0 +1,21 @@
+
+
+const ForlawCards = ({ img, name }: { img: string; name: string}) => {
+  return (
+    <div className="group hover:bg-[#1e3a8a] shadow-md p-4 rounded-md flex flex-col items-center transition duration-300">
+      <div className="mb-4">
+        <img
+          src={img}
+          alt={name}
+          width={30}
+          className=""
+        />
+      </div>
+      <div className="text-lg font-medium text-black group-hover:text-white text-center cursor-pointer">
+        {name}
+      </div>
+    </div>
+  );
+};
+
+export default ForlawCards;
